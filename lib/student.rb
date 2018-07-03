71ac54c951901690e07d0a8eb4e7f585ebbd407a
Hillevi Eklöw <hillevieklow@gmail.com>
2018-07-03 18:43:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,11 +9,12 @@ class Student
   end
 
   def self.create_table
-    sql = "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)"
+    sql = "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT);"
     DB[:conn].execute(sql)
   end
 
   def self.drop_table
+    sql = "DROP TABLE students;"
   end
 
   def save
